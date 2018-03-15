e697a359b60db5a7e9ee7bf072224089e933ae94
ruslanvs <ruslanvs@yahoo.com>
2018-03-14 18:59:39 -0700

    Assignment complete

---
+++ custom_cell/CustomCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomCell.swift
+//  custom_cell
+//
+//  Created by Ruslan Suvorov on 3/14/18.
+//  Copyright © 2018 Ruslan Suvorov. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class CustomCell: UITableViewCell {
+    
+    @IBOutlet weak var rightLabel: UILabel!
+    @IBOutlet weak var leftButton: UIButton!
+    
+}
---
+++ custom_cell/CustomCellVC.swift
@@ -0,0 +1,99 @@
+//
+//  CustomCellVC.swift
+//  custom_cell
+//
+//  Created by Ruslan Suvorov on 3/14/18.
+//  Copyright © 2018 Ruslan Suvorov. All rights reserved.
+//
+
+import UIKit
+
+class CustomCellVC: UITableViewController {
+    
+    var nums = [1, 90, 32, 23, 9, 12]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nums.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell", for: indexPath) as! CustomCell
+        
+        cell.rightLabel.text = "\( nums[indexPath.row] )"
+        
+        if nums[indexPath.row] > 24 {
+            cell.leftButton.backgroundColor = UIColor.green
+        } else {
+            cell.leftButton.backgroundColor = UIColor.red
+        }
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
